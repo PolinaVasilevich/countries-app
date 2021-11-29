9dc94885cab453074933a4db486da89a6b3b25a4
PolinaVasilevich <polina.vasilevichh@gmail.com>
2021-11-29 13:39:52 +0300

    added new functionalities

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,35 @@
-import React from "react";
-import { useParams } from "react-router";
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router";
+
+import { IoArrowBack } from "react-icons/io5";
+import axios from "axios";
+import { searchByCountry } from "../config";
+import { Button } from "../components/Button";
+import Info from "../components/Info";
 
 const Details = () => {
   const { name } = useParams();
-  return <div>Details {name}</div>;
+  const navigate = useNavigate();
+
+  const [coutry, setCountry] = useState(null);
+
+  const getDetailtsCountry = async () => {
+    const response = await axios.get(searchByCountry(name));
+    setCountry(response.data[0]);
+  };
+
+  useEffect(() => {
+    getDetailtsCountry();
+  }, [name]);
+
+  return (
+    <div>
+      <Button onClick={() => navigate(-1)}>
+        <IoArrowBack /> Back
+      </Button>
+      {coutry && <Info {...coutry} push={navigate} />}
+    </div>
+  );
 };
 
 export default Details;
